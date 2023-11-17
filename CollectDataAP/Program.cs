23b3095e60d5f3fa598db210869925098f4778f5
testtestproblem <tommyapple321@gmail.com>
2023-11-17 12:30:16 +0800

    Update - It can UWP and  console can request and respond data.

--- CollectDataAP/Program.cs
+++ CollectDataAP/Program.cs
@@ -3,13 +3,144 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.AppService;
+using Windows.Foundation.Collections;
 
 namespace CollectDataAP
 {
     class Program
     {
+
+        static private AppServiceConnection connection = null;
+
+
         static void Main(string[] args)
         {
+            InitializeAppServiceConnection();
+
+            string s_res;
+
+            Console.WriteLine("[1] send data to UWP");
+            while ((s_res = Console.ReadLine()) != "")
+            {
+                int i_res=int.Parse(s_res);
+
+                if (i_res == 1)
+                {
+                    Send2UWP_2("Hi!", "UWP");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Open connection to UWP app service
+        /// </summary>
+        static private async void InitializeAppServiceConnection()
+        {
+            connection = new AppServiceConnection();
+            connection.AppServiceName = "SampleInteropService";
+            connection.PackageFamilyName = Package.Current.Id.FamilyName;
+            connection.RequestReceived += Connection_RequestReceived;
+            connection.ServiceClosed += Connection_ServiceClosed;
+
+            AppServiceConnectionStatus status = await connection.OpenAsync();
+            if (status != AppServiceConnectionStatus.Success)
+            {
+                //In console, something wrong
+                Console.WriteLine(status.ToString());
+                //In app, something went wrong ...
+                //MessageBox.Show(status.ToString());
+                //this.IsEnabled = false;
+            }
+        }
+
+        /// <summary>
+        /// Handles the event when the desktop process receives a request from the UWP app
+        /// </summary>
+        static private async void Connection_RequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+            // retrive the reg key name from the ValueSet in the request
+            /*
+            string key = args.Request.Message["KEY"] as string;
+            if (key == "abcd")
+            {
+                // compose the response as ValueSet
+                ValueSet response = new ValueSet();
+                response.Add("GOOD", "KEY IS FOUNDED ^^");
+
+                // send the response back to the UWP
+                await args.Request.SendResponseAsync(response);
+            }
+            else
+            {
+                ValueSet response = new ValueSet();
+                response.Add("ERROR", "INVALID REQUEST");
+                await args.Request.SendResponseAsync(response);
+            }
+            */
+            int? key1 = args.Request.Message["KEY1"] as int?;
+            int? key2 = args.Request.Message["KEY2"] as int?;
+
+            if (key1 != null && key2 != null)
+            {
+                int ans = (int)key1 + (int)key2;
+
+                // compose the response as ValueSet
+                ValueSet response = new ValueSet();
+                response.Add("KEY3", ans);
+
+                // send the response back to the UWP
+                await args.Request.SendResponseAsync(response);
+            }
+        }
+
+        /// <summary>
+        /// Handles the event when the app service connection is closed
+        /// </summary>
+        static private void Connection_ServiceClosed(AppServiceConnection sender, AppServiceClosedEventArgs args)
+        {
+            //In console, connection to the UWP lost, so we shut down the desktop process
+            Console.WriteLine("UWP lost connection! Please restart.");
+            Console.ReadLine();
+            Environment.Exit(0);
+            //In app, connection to the UWP lost, so we shut down the desktop process
+            //Dispatcher.BeginInvoke(DispatcherPriority.Normal, new Action(() =>
+            //{
+            //    Application.Current.Shutdown();
+            //}));
+        }
+
+        static private async void Send2UWP(double a, double b)
+        {
+            // ask the UWP to calculate d1 + d2
+            ValueSet request = new ValueSet();
+            request.Add("D1", a);
+            request.Add("D2", b);
+
+            //start sending
+            AppServiceResponse response = await connection.SendMessageAsync(request);
+            //get response
+            double result = (double)response.Message["RESULT"];
+
+            Console.WriteLine(result.ToString());
+        }
+
+        static private async void Send2UWP_2(string a, string b)
+        {
+            // ask the UWP to calculate d1 + d2
+            ValueSet request = new ValueSet();
+            request.Add("s_a", a);
+            request.Add("s_b", b);
+
+            //start sending
+            AppServiceResponse response = await connection.SendMessageAsync(request);
+            //get response
+            string result = response.Message["toConsole_result"] as string;
+
+            Console.WriteLine("send data_a to UWP: " + a);
+            Console.WriteLine("send data_b to UWP: " + b);
+            Console.WriteLine("getting data from UWP: " + result);
         }
     }
 }
--- HotTab_Win10/MainPage.xaml.cs
+++ HotTab_Win10/MainPage.xaml.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.AppService;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Foundation.Metadata;
+using Windows.UI.Core;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +30,111 @@ namespace HotTab_Win10
         public MainPage()
         {
             this.InitializeComponent();
+
+            // ApplicationView.PreferredLaunchViewSize = new Size(200, 200);
+            // ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            if (ApiInformation.IsApiContractPresent("Windows.ApplicationModel.FullTrustAppContract", 1, 0))
+            {
+                //for connect or disconnect event
+                App.AppServiceConnected += MainPage_AppServiceConnected;
+                App.AppServiceDisconnected += MainPage_AppServiceDisconnected;
+
+                //ApplicationData.Current.LocalSettings.Values["parameters"] = "test";
+
+                //for sideload app
+                await FullTrustProcessLauncher.LaunchFullTrustProcessForCurrentAppAsync();
+            }
+        }
+
+
+        /// <summary>
+        /// When the desktop process is connected, get ready to send/receive requests
+        /// </summary>
+        private async void MainPage_AppServiceConnected(object sender, AppServiceTriggerDetails e)
+        {
+            App.Connection.RequestReceived += AppServiceConnection_RequestReceived;
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                // enable UI to access  the connection
+                //btnRegKey.IsEnabled = true;
+            });
+        }
+
+        /// <summary>
+        /// Handle calculation request from desktop process
+        /// (dummy scenario to show that connection is bi-directional)
+        /// </summary>
+        private async void AppServiceConnection_RequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+            //get value from sideload app
+            string s_a = (string)args.Request.Message["s_a"];
+            string s_b = (string)args.Request.Message["s_b"];
+            //double result = d1 + d2;
+
+            //send "have get data" to sideload app
+            ValueSet response = new ValueSet();
+            response.Add("toConsole_result", "Hello! sideload app");
+            await args.Request.SendResponseAsync(response);
+
+            //textBox.Text = ""; //it will error
+            //log the getting value in the UI for demo purposes
+            await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>     //I don't know this code
+            {
+                textBox.Text += string.Format("Request(getting data from sideload app):\ndata 1: {0} \ndata 2: {1}", s_a, s_b);
+            });
+        }
+
+
+        /// <summary>
+        /// When the desktop process is disconnected, reconnect if needed
+        /// </summary>
+        private async void MainPage_AppServiceDisconnected(object sender, EventArgs e)
+        {
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                Reconnect();
+            });
+        }
+
+        /// <summary>
+        /// Ask user if they want to reconnect to the desktop process
+        /// </summary>
+        private async void Reconnect()
+        {
+            if (App.IsForeground)
+            {
+                MessageDialog dlg = new MessageDialog("Connection to desktop process lost. Reconnect?");
+                UICommand yesCommand = new UICommand("Yes", async (r) =>
+                {
+                    await FullTrustProcessLauncher.LaunchFullTrustProcessForCurrentAppAsync();
+                });
+                dlg.Commands.Add(yesCommand);
+                UICommand noCommand = new UICommand("No", (r) => { });
+                dlg.Commands.Add(noCommand);
+                await dlg.ShowAsync();
+            }
+        }
+
+        private async void calc_btn_Click(object sender, RoutedEventArgs e)
+        {
+            //send value to sideloade app
+            ValueSet request = new ValueSet();
+            request.Add("KEY1", int.Parse(a_textBlock.Text));
+            request.Add("KEY2", int.Parse(b_textBlock.Text));
+            AppServiceResponse response = await App.Connection.SendMessageAsync(request);   //send data and get response 
+
+            //display the response key/value pairs
+            //tbResult.Text = "";
+            foreach (string key in response.Message.Keys)
+            {
+                ans_textBox.Text = "sended by sideload app\nkey: " + key + "\nvalue: " + response.Message[key];
+            }
         }
     }
 }
